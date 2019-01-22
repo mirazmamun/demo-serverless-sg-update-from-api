cc4b3590f20fab36db8c396ad5d74c6025de0bb1
Miraz <mirazmamun@yahoo.com>
2019-01-22 20:30:37 +1100

    fix(script): library and handler update for handling the backend handling

--- tests/unit/test-SG-spec.js
+++ tests/unit/test-SG-spec.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const expect = require('chai').expect;
 const should = require('chai').should();
 const SG = require('../../lib/SG');
-it.only('should find an existing SG', function () {
+it('should find an existing SG', function () {
     SG.findSG({sgName: SG.defaults().DEFAULT_SG_NAME})
         .then((res) => {
             console.log(res);
@@ -20,6 +20,16 @@ it('should construct valid ingress rules', function () {
 });
 it('should create or update SG with rules', function () {
     SG.createOrupdateSGRule(SG.defaults().DEFAULT_SG_NAME, [ { protocol: 'tcp', IP: '8.8.8.8', port: 443 } ])
+        .then((res) => {
+            console.log(res);
+            expect(res).to.be.ok;
+        })
+        .catch((err) => {
+            expect(err).to.be.undefined;
+        })
+});
+it.only('should revoke all ingress rules', function () {
+    SG.removeSG(SG.defaults().DEFAULT_SG_NAME)
         .then((res) => {
             console.log(res);
             expect(res).to.be.ok;
